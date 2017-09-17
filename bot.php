b7f619ccbfd58c5b5456455a0b075579b4a1750c
ModelZ <32031595+ModelZ@users.noreply.github.com>
2017-09-17 22:13:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,8 +12,8 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		
-		if($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['text'] == 'หยุดไอ้บอท'){
-		break;
+	if($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['text'] == 'หยุดไอ้บอท'){
+		$stop = 1
 	}
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $stop != 1) {
 			// Get text sent
