ad437f9301aa7fdab09ddb71fe44068c32a14678
ModelZ <32031595+ModelZ@users.noreply.github.com>
2017-09-17 22:49:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-//$access_token = '6W2OTEsmOrywElp+4VOXXOhvkRK8JzxpMLpD/Ev/6r5JpOhjP3CR+s9l0rrj6sRziSHyq5qTtNEnFavVecf4BwV+jZ0BvFhCb13unniqRMhfnBFL8/l0OLQYNGjw6wzjVT65ZVWNKJnj3TRjSngnIAdB04t89/1O/w1cDnyilFU=';
-$access_token = '';
+isset($_SESSION['stop'])?$access_token = NULL:$access_token = '6W2OTEsmOrywElp+4VOXXOhvkRK8JzxpMLpD/Ev/6r5JpOhjP3CR+s9l0rrj6sRziSHyq5qTtNEnFavVecf4BwV+jZ0BvFhCb13unniqRMhfnBFL8/l0OLQYNGjw6wzjVT65ZVWNKJnj3TRjSngnIAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -17,7 +16,7 @@ if (!is_null($events['events'])) {
 		$_SESSION['stop'] = 1;
 	}
 		
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && !isset($_SESSION['stop'])) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text']."ใจดี";
 			// Get replyToken
@@ -48,7 +47,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}else if( $_SESSION['stop'] != 1){
+		}else{
 			$text = "ส่งสติ้กมาทำไม??";
 			
 			$replyToken = $event['replyToken'];
