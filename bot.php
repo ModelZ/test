5291ef88f94673cea6fa316545e109f8791235a2
ModelZ <32031595+ModelZ@users.noreply.github.com>
2017-09-20 21:40:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 			$event['message']['text'] = str_replace("รึเปล่าครับ","รึเปล่าคะ",$event['message']['text']);
 			$event['message']['text'] = str_replace("ไหมครับ","ไหมคะ",$event['message']['text']);
 			$event['message']['text'] = str_replace("ครับ","ค่ะ",$event['message']['text']);
-			$event['message']['text'] = str_replace("ผม","หนู",$event['message']['text']);
+			$event['message']['text'] = str_replace("ผม,กู","หนู",$event['message']['text']);
 			$ch = $event['message']['text'];
 			switch($ch){
 				case "พี่ไวท์" : $text = "พี่ไวท์ผู้กินมากเกินไป";
